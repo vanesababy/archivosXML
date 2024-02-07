3054c54a9d8939c5c228be24fb04ed06e6143680
JESSICA VANESA ACOSTA GALINDEZ <auxiliartics5agssalud@gmail.com>
2024-02-07 11:19:01 -0500

    adjunto archivo xml y ajuste rta redirect

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('index');
+    return redirect(route('index'));
 });
 Route::post('/xml/upload', [xmlController::class, 'upload'])->name('xml.upload');
 Route::get('index', [xmlController::class, 'index'])->name('index');
